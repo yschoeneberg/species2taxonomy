820668550f00355c1431c00508333363d09c37d2
Yannis Schöneberg <98947919+yschoeneberg@users.noreply.github.com>
2024-06-20 17:10:52 +0200

    Create species2taxonomy.py
    Adapted from the blast2taxonomy script

---
+++ species2taxonomy.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+# blast2taxonomy.py
+# Author: Yannis Schöneberg <yannis.schoeneberg@gmx.de>
+# This script takes in a blast result table and outputs the taxonomy data in a tsv file
+# Version 0.1.0
+import getopt
+import sys
+import os
+import logging
+import pandas as pd
+from multiprocessing import Pool
+from ete3 import NCBITaxa
+from itertools import repeat
+
+
+def get_options(argv):
+    global version
+    global skip_update
+    global skip_failed
+    global threads
+    global ranks
+    global tax_column
+    global perc_column
+    global len_column
+    global fail_file
+    version = "0.1.0"
+    skip_update = False
+    skip_failed = False
+    ranks = ['kingdom', 'phylum', 'superclass', 'class', 'subclass', 'order', 'infraorder', 'superfamily', 'family', 'genus', 'species']
+    fail_file = "failed_taxids.tsv"
+    
+    try:
+        opts, args = getopt.getopt(argv, "hfsi:o", ["ifile=", "ofile="])
+    except getopt.GetoptError:
+        print(f"Usage: blast2taxonomy_v{str(version)}.py -i <infile> -o <outfile> -c <column taxids> -t <num threads>\n"
+              f"Type blast2taxonomy_v{str(version)}.py -h for help")
+        
+    for opt, arg in opts:
+        if opt == '-h':
+            print(f"\nUsage: blast2taxonomy_v{str(version)}.py [options]\n"
+                  f"Version: {str(version)}\n"
+                  f"\n"
+                  f"REQUIRED:\n"
+                  f"\t-i\tFile containing the species list with one species per line.\n"
+                  f"\t-o\tOutput file\n"
+                  f"\n"
+                  f"OPTIONAL:\n"
+                  f"\t-r\tComma seperated list of taxonomic ranks to extract. Must be ranks of the NCBI-Taxonomy.\n
+                  f"\t-s\tSkip Taxonomy Database Update\n"
+                  f"\t-f\tSkip failed taxIDs and write those to 'failed_taxids.tsv'"
+                  f"\t-h\tDisplay this help message\n"
+                  f"\n")
+            exit()
+        elif opt == '-i':
+            global species_infile
+            species_infile = arg
+        elif opt == '-o':
+            global outfile
+            outfile = arg
+        elif opt == "-f":
+            skip_failed = True
+        elif opt == "-s":
+            skip_update = True
+            try:
+                os.remove(fail_file)
+            except FileNotFoundError:
+                pass
+            except Exception as e:
+                raise e
+        elif opt == "-r":
+            ranks = arg.split(",")
+
+
+def update_taxdb():
+    logger.info(f"### Updating Taxonomy Database")
+    ncbi.update_taxonomy_database()
+    logger.info(f"Finished Updating Database")
+    logger.info(f"Removing temporary files")
+    os.remove("taxdump.tar.gz")
+
+
+def get_taxonomy (species, ranks):
+
+    tax_annotations = []
+    for spec in species:
+        lineage2ranks = ncbi.get_rank(species)
+        ranks2lineage = dict((rank, species) for (species, rank) in lineage2ranks.items())
+        desired_taxids = [ranks2lineage.get(rank, 'Nan') for rank in ranks]
+        taxonomy = ncbi.get_taxid_translator(lineage)
+        tax_inf = [taxonomy.get(species, 'Nan') for taxid in desired_taxids]
+        tax_annotations.append(tax_inf)
+    transp_taxonomy = list(zip(*tax_annotations))
+    return [species[0]] + transp_taxs
+
+
+if __name__ == '__main__':
+    logger = logging.getLogger('my_logger')
+    my_handler = logging.StreamHandler()
+    my_formatter = logging.Formatter("%(asctime)s - %(levelname)s - %("
+                                     "message)s")
+    my_handler.setFormatter(my_formatter)
+    logger.addHandler(my_handler)
+    logger.setLevel(logging.INFO)
+
+    get_options(sys.argv[1:])
+
+    logger.info(f"#### Extracting Taxonomy Information for Blast Results\n"
+                f"{'Program Version:':<50} {str(version)}\n"
+                f"{'Species List:':<50} {species_infile}\n"
+                f"{'Output file:':<50} {outfile}\n"
+                f"{'Skip Taxonomy DB update:':<50} {skip_update}\n"
+                f"{'Skip failed Species:':<50} {skip_failed}\n"
+    species = pd.read_csv(species_infile, sep="\t", dtype=str, header=None)
+    species = species.values.tolist()
+    global ncbi
+    ncbi = NCBITaxa()
+    if skip_update is False:
+        update_taxdb()
+    else:
+        logger.info("Skipping Taxonomy Database Update")
+
+    logger.info(f"Searching TaxIDs vs Taxonomy DB")
+    taxlist = get_taxonomy(species, ranks)
+    taxlist = [i for i in taxlist if i is not None]
+
+    logger.info(f"Writing Taxonomy Information to: {outfile}")
+    pd.DataFrame(taxlist, columns=ranks).to_csv(outfile, sep="\t", index=False)
+    logger.info(f"Done")
